5cb0038fe3b411736f5cf90eb134c33464f56afe
George <tsatsarisgiorgos@gmail.com>
2025-10-11 22:09:19 +0300

    feat: workout handler

---
+++ internal/api/workout_handler.go
@@ -0,0 +1,37 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type WorkoutHandler struct {
+}
+
+func NewWorkoutHandler() *WorkoutHandler {
+	return &WorkoutHandler{}
+}
+
+func (wh *WorkoutHandler) HandleGetWorkoutByID(w http.ResponseWriter, r *http.Request) {
+	paramsWorkoutID := chi.URLParam(r, "id")
+	if paramsWorkoutID == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	workoutID, err := strconv.ParseInt(paramsWorkoutID, 10, 64)
+	if err != nil || workoutID <= 0 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Workout ID: %d\n", workoutID)
+}
+
+func (wh *WorkoutHandler) HandleCreateWorkout(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create Workout endpoint")
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -5,17 +5,26 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/tsatsarig/go-project/internal/api"
 )
 
 type Application struct {
-	Logger *log.Logger
+	Logger         *log.Logger
+	WorkoutHandler *api.WorkoutHandler
 }
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
+	// stores
+
+	// handlers
+	workoutHandler := api.NewWorkoutHandler()
+
 	app := &Application{
-		Logger: logger,
+		Logger:         logger,
+		WorkoutHandler: workoutHandler,
 	}
 
 	return app, nil
